5d32e5cbd22f705b2d2b4237e5058b624dbb282e
khoilv1 <khoilv@meeyland.com>
2023-03-02 16:33:28 +0700

    init

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require('http');
-
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+  };
 //create a server object:
 http.createServer(function (req, res) {
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+      }
+      res.writeHead(200, headers);
+
   res.write(`${Date.now()}`);
   res.end();
 }).listen(8080); 
