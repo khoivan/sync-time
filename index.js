efc69964396dce3235ae33fe0ffcc8a40997da64
khoilv1 <khoilv@meeyland.com>
2023-03-03 12:58:11 +0700

    format

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var http = require('http');
+var http = require("http");
 const headers = {
-    'Access-Control-Allow-Origin': '*',
-  };
+  "Access-Control-Allow-Origin": "*",
+};
 //create a server object:
-http.createServer(function (req, res) {
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204, headers);
-        res.end();
-        return;
-      }
-      res.writeHead(200, headers);
+http
+  .createServer(function (req, res) {
+    if (req.method === "OPTIONS") {
+      res.writeHead(204, headers);
+      res.end();
+      return;
+    }
+    res.writeHead(200, headers);
 
-  res.write(`${Date.now()}`);
-  res.end();
-}).listen(8080); 
+    res.write(`${Date.now()}`);
+    res.end();
+  })
+  .listen(8080);
